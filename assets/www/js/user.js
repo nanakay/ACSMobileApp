8563b4cadce19a358b8e4d0d1e2e151b62325a7d
nanakay <christian.osei-bonsu@meltwater.org>
2013-05-07 00:52:55 +0000

    After enabling feedback

--- assets/www/js/user.js
+++ assets/www/js/user.js
@@ -18,12 +18,6 @@ else {
 var number; 
 var firstName;
 function initAll() {
-//	startBtnArray = getByClass("start_btn");
-	
-//	for (var i = 0; i < startBtnArray.length; i++) {
-////		alert(startBtnArray[i]);
-//		startBtnArray[i].onclick = Navigate;
-//	}
     number = document.getElementById("phone_number");
     firstName = getById("firstName");
 	var login_btn = document.getElementById("login_btn");
@@ -37,8 +31,7 @@ function initAll() {
 }
 
 function verify_user(evt) {
-    //alert("Verifying User");
-	evt.preventDefault();
+//	evt.preventDefault();
 	var phonenumber = document.getElementById('in_phone_number').value;
 	var password = document.getElementById('in_password').value;
 //	console.log(phonenumber, password);
@@ -79,7 +72,8 @@ function verify_user(evt) {
                     
                     getById("welcome_user").innerHTML = "Welcome " + firstName.value;
                     
-                    window.location.href = passenger_url;
+                    window.location.replace(passenger_url);
+//                    window.location.href = passenger_url;
 	            }
 	        }
 	    }
@@ -88,7 +82,7 @@ function verify_user(evt) {
 	    xhttp.send();
 	}
 	else {
-	    var error = "Both phone number and password required";
+	    var error = "Phone number and password required";
         var el = document.getElementById("login_error");
         el.innerHTML = error;
         el.style.display = "block";
@@ -96,14 +90,13 @@ function verify_user(evt) {
 	
 }
 
-//function Navigate (evt) {
-//	var el = evt.target;
-//	var link = ascendDom(el, "a");
-//	var url = link.href;
-//	
-////	console.log(url);
-//	window.location = url;
-//}
+function loginNavigate () {
+    window.location.replace("file:///android_asset/www/index.html#login");
+}
+
+function signupNavigate () {
+    window.location.replace("file:///android_asset/www/index.html#signup");
+}
 
 function createUser(evt) {
 	evt.preventDefault();
@@ -168,36 +161,192 @@ function validateEmail (email) {
 }
 
 function requestCab() {
-//    alert("I'm here to request for a cab");
     current_location = getById("request_location").value;
     destination = getById("request_destination").value;
     pickup_time = getById("pickup_time").value;
-    time_frame = getById("timeframe").value;
-    time_label = getById("timeframe_option").value;
-    other_info = getById("other_info");
+//    time_frame = getById("timeframe").value;
+//    time_label = getById("timeframe_option").value;
+    other_info = getById("other_info").value;
     
-    timeFrame = time_frame + " " + time_label;
+//    timeFrame = time_frame + " " + time_label;
     
-    if (current_location && destination && pickup_time) {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            var resp = xhttp.responseText;
-            if (resp === "successful") {
-                var current_url = window.location.href;
-                var passenger_url = current_url.substring(0, current_url.lastIndexOf("/"))+ "#request-confirmation";
-                window.location.href = passenger_url;
+    if (current_location && destination && pickup_time && number.value) {
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+    //          alert(xhttp.readyState + " " + xhttp.status)
+                var resp = xhttp.responseText;
+                
+                if (resp === "successful") {
+                    var current_url = window.location.href;
+                    var passenger_url = current_url.substring(0, current_url.lastIndexOf("#"))+ "#request-confirmation";
+                    window.location.href = passenger_url;
+                }
+                else {
+                    var error = "Something went wrong";
+                    var error_el = getById("request_error");
+                    error_el.innerHTML = error;
+                    error_el.style.display = "block"
+                }
+                
             }
         }
-        
-        var url = 'http://cabkonekt.appspot.com/request?current_location=' + current_location + '&destination=' + destination + "&time=" + pickup_time + "&timeframe=" + timeframe + "&phone_number=" + number;
-        xhttp.open('POST', url, true);
+        var url = 'http://cabkonekt.appspot.com/request?option=request_cab' + '&current_location=' + current_location + '&destination=' + destination + "&time=" + pickup_time + "&phone_number=" + number.value + "&other_info=" + other_info;
+        xhttp.open('GET', url, true);
         xhttp.send();
     }
     
     else {
-        var error = "All data are required please";
-        var error_el = mgetById("request_error").style.display = "block";
+        var error = "Some required data have not been provided";
+        var error_el = getById("request_error");
         error_el.innerHTML = error;
+        error_el.style.display = "block";
     }
+}
+
+function reserveCab() {
+    alert("reserve");
+    current_location = getById("reserve_location").value;
+    destination = getById("reserve_destination").value;
+    from_date = getById("from_date").value;
+    to_date = getById("to_date").value;
+    pickup_time = getById("reserve_pickup_time").value;
+    to_time = getById("reserve_to_time").value;
+    passengers = getById("total_passengers").value;
+    other_info = getById("reserve_other_info").value;
+    
+    if (current_location && destination && from_date && to_date && pickup_time && to_time && passengers) {
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+    //          alert(xhttp.readyState + " " + xhttp.status)
+                var resp = xhttp.responseText;
+                alert(resp);
+                if (resp === "successful") {
+                    var current_url = window.location.href;
+                    var passenger_url = current_url.substring(0, current_url.lastIndexOf("#"))+ "#request-confirmation";
+                    window.location.href = passenger_url;
+                }
+                else {
+                    var error = "Please check your internet connection";
+                    getError("request_error", error)
+                }
+                
+            }
+        }
+        var url = 'http://cabkonekt.appspot.com/request?option=reserve_cab' + '&current_location=' + current_location + '&destination=' + destination + "&from_date=" + from_date + "&to_date=" + to_date + "&reserve_pickup_time=" + pickup_time + "&to_time=" + to_time + "&phone_number=" + number.value + "&total_passengers=" + passengers + "&other_info=" + other_info;
+        xhttp.open('GET', url, true);
+        xhttp.send();
+    }
+    else {
+        var error = "Some required data have not been provided";
+        getError("reserve_error", error);
+    }
+    
+}
+
+function getError(elId, error) {
+    var error_el = getById(elId); 
+    error_el.innerHTML = error;
+    error_el.style.display = "block";
+}
+
+function checkHistory() {
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+//          alert(xhttp.readyState + " " + xhttp.status)
+            var resp = xhttp.responseText;
+//            alert(resp);
+            if (resp) {
+                var current_url = window.location.href;
+                var passenger_url = current_url.substring(0, current_url.lastIndexOf("#"))+ "#check-history";
+                
+                window.console.log(resp);
+                var history = JSON.parse(resp);
+                
+                var brk = document.createElement("br")
+                history_elem = getById("history_list");
+                for (var i =0; i < history.length; i++) {
+                    var list_member = document.createElement("li");
+                    
+                    var aTag = document.createElement("a");
+                    aTag.setAttribute("href", "");
+                    var data = document.createTextNode("You requested for a cab from " + history[i].request_location + " to " + history[i].request_destination + " On " + history[i].request_date
+                            + ", the driver was " + history[i].driver); //+ " to " + history[i].request_destination + brk + "On " history[i].created + ", the driver was " + history[i].driver);
+                    
+                    list_member.appendChild(data);
+//                  aTag.appendChild(data);
+                    history_elem.appendChild(list_member);
+                }
+                
+//                history_elem.innerHTML = history[0].request_location;
+                
+                window.location.href = passenger_url;
+            }
+            else {
+                var error = "Something went wrong";
+                getError("request_error", error)
+            }
+            
+        }
+    }
+    var url = "/history?phone_number=" + number.value;
+    xhttp.open('GET', url, true);
+    xhttp.send();
+}
+
+function sendFeedback() {
+    var price_value = getRateValue ("price");
+    var punctuality_value = getRateValue ("punctuality");
+    var security_value = getRateValue ("security");
+    var care_value = getRateValue ("customercare");
+    var standard_value = getRateValue ("standard");
+    
+    var text_feedback = getById("text_feedback").value;
+    
+    if (price_value || punctuality_value || security_value || care_value || standard_value || text_feedback && number.value) {
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+            alert(xhttp.readyState + " " + xhttp.status)
+                var resp = xhttp.responseText;
+//              alert(resp);
+                if (resp === "successful") {
+                    var current_url = window.location.href;
+                    var passenger_url = current_url.substring(0, current_url.lastIndexOf("#"))+ "#feedback-thanks";
+                    
+                    alert(current_url);
+                    alert(passenger_url);
+                    window.location.href = passenger_url;
+                }
+                else {
+                    var error = "Could not connect to the server";
+                    getError("feedback_error", error)
+                }
+                
+            }
+        }
+        var url = "http://cabkonekt.appspot.com/feedback?phone_number=" + number.value + "&price_rating=" + price_value + "&punctuality_rating=" + punctuality_value + "&security_rating=" + security_value + "&care_rating=" + care_value + "&standard_rating=" + standard_value + "&message=" + text_feedback;
+        xhttp.open('POST', url, true);
+        xhttp.send();
+    }
+    else {
+        var error = "No feedback chosen or entered to send";
+        getError("feedback_error", error)
+    }
+}
+
+function getRateValue(name) {
+var checks = document.getElementsByName(name);
+var value;
+    
+    for (var i = 0; i < checks.length; i ++) {
+        if (checks[i].checked) {
+            value = checks[i].value;
+            break;
+        }
+        else {
+            value = 0;
+        }
+    }
+    return value;
 }
 
 
